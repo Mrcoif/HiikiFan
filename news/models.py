fd317469b368c104fa60a0dfb91e6df74f0a30f9
Mrcoif <svzagarov@gmail.com>
2024-02-06 12:26:26 +0600

    add news page add create news page add update news page

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=250)
@@ -8,3 +9,10 @@ class Articles(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return f'/news/{self.id}'
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
--- news/views.py
+++ news/views.py
@@ -1,5 +1,45 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import DetailView, UpdateView
+
+from .models import Articles
+from .forms import ArticlesForm
 
 
 def news_home(request):
-    return render(request, 'news/news_home.html')
+    news = Articles.objects.order_by('-date')
+    return render(request, 'news/news_home.html', {'news': news})
+
+
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
+class NewsUpdateView(UpdateView):
+    model = Articles
+
+    form_class = ArticlesForm
+
+    template_name = 'news/create.html'
+    context_object_name = 'article'
+
+
+def create(request):
+    error = ''
+    if request.method == 'POST':
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('news_home')
+        else:
+            error = 'Форма неверно заполнена'
+    else:
+        form = ArticlesForm()
+
+    data = {
+        'form': form,
+        'error': error,
+    }
+
+    return render(request, 'news/create.html', data)
